cac9755c91c751a2b932682f2cba0ffec2a9fed7
khokharkeyur <keyurkhokhar04@gmail.com>
2024-11-14 20:54:44 +0530

    try to access token in ProtectedRoute

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,7 +1,11 @@
 import { Navigate, Outlet } from 'react-router-dom';
+import Cookies from 'cookies-js'; 
 
 const PrivateRoutes = () => {
   const auth = { token: true };
+  const token = Cookies.get('token');
+  console.log('Token:', token); 
+  
 
   return (
     auth.token ? <Outlet /> : <Navigate to="/login" />
