3730ce28b4311171b190415478109b93bb8bdd71
khokharkeyur <keyurkhokhar04@gmail.com>
2024-11-18 21:15:24 +0530

    pass refreshToken to user login

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -157,9 +157,11 @@ export const login = async (req, res) => {
         };
 
         const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
+        const refreshToken = jwt.sign(tokenData, process.env.JWT_SECRET_KEY,{ expiresIn:'7d' });
 
         return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' }).json({
             token,
+            refreshToken,
             _id: user._id,
             username: user.username,
             fullName: user.fullName,
