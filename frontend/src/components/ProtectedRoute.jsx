30b0b67de11a8f79e0a335aa680ffe03eadfd031
khokharkeyur <keyurkhokhar04@gmail.com>
2024-11-22 21:13:12 +0530

    Update PrivateRoutes to retrieve AccessToken` from Cookies

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,7 +1,8 @@
 import { Navigate, Outlet } from "react-router-dom";
+import Cookies from "js-cookie";
 
 const PrivateRoutes = () => {
-  const token = localStorage.getItem("Token");
+  const token =  Cookies.get("AccessToken");
   
   return token ? <Outlet /> : <Navigate to="/login" />;
 };
