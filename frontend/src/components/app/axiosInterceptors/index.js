4cf69289f0dfb90cba34aaf1d4490283a509f49a
khokharkeyur <keyurkhokhar04@gmail.com>
2024-11-29 18:30:50 +0530

    add Axios interceptors

---
+++ frontend/src/components/app/axiosInterceptors/index.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const baseURL = "http://localhost:8000/api";
+
+const axiosInterceptors = axios.create({
+  baseURL: baseURL,
+});
+
+axiosInterceptors.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("AccessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInterceptors.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      Cookies.remove("AccessToken");
+    }
+    console.error("this error is ", error);
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInterceptors;
