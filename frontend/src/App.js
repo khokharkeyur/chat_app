6ae6d94153026fe31ca4c0af5b6e539983d193fc
khokharkeyur <keyurkhokhar04@gmail.com>
2025-04-02 06:57:09 +0530

    Add OTP verification to the Forgot Password

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,11 +19,11 @@ const router = createBrowserRouter([
         path: "/",
         element: <Dashboard />,
       },
-      {
-        path: "/forgetPassword",
-        element: <ForgetPassword />,
-      },
     ],
+  },
+  {
+    path: "/forgetPassword",
+    element: <ForgetPassword />,
   },
   {
     path: "/signup",
